a30b41a6eedeb9acf10dc4ab75fbcb504b6094b6
grayzerguy <grayzerg@gmail.com>
2022-11-23 23:22:57 +0200

    add

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from "./Data";
+import List from "./List";
 
-function App() {
+// useState<ProductModel[]>([]);
+function App(): JSX.Element {
+  const [people, setPeople] = useState(data);
+  const clear = (): void => {
+    setPeople([]);
+  };
+  const getAllBack = (): void => {
+    setPeople(data);
+  };
+  // render the list of people automatically
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        {people.length > 0 && <button onClick={clear}>Clear All</button>}
+
+        {people.length === 0 && (
+          <button onClick={getAllBack}>Get All Back</button>
+        )}
+        {/* <button onClick={getAllBack}>Get All</button> */}
+      </section>
+    </main>
   );
 }
 
